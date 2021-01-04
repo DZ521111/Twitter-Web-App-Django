7184dbd0cda2a6de214e6416a333788a134c33e1
DZ521111 <dhruvkakadiya3@gmail.com>
2021-01-04 23:30:09 +0530

    settings.py updated!

--- TwitterWebApp/TwitterWebApp/settings.py
+++ TwitterWebApp/TwitterWebApp/settings.py
@@ -129,8 +129,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'assets')
-#MEDIA_URL = '/assets/'
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 

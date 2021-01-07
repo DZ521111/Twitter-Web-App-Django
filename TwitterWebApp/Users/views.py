bd51ae8b0e3a53adb165c8386dd875149b2ca141
DZ521111 <dhruvkakadiya3@gmail.com>
2021-01-07 20:47:20 +0530

    views.py code updated!

--- TwitterWebApp/Users/views.py
+++ TwitterWebApp/Users/views.py
@@ -17,3 +17,14 @@ def login(request):
 
 
 # for registration purpose here we create register method
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}.')
+            return redirect('login')
+    else:
+        form = RegistrationForm()
+    return render(request, 'all_users_pages/register.html', {'form': form})

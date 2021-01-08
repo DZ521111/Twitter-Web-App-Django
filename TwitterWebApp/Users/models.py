3539a9da3cf60f586ad2bd1be859e8e56b95ae85
DZ521111 <dhruvkakadiya3@gmail.com>
2021-01-08 18:29:57 +0530

    Users/ models.py updated!

--- TwitterWebApp/Users/models.py
+++ TwitterWebApp/Users/models.py
@@ -23,4 +23,6 @@ class User_Profile(models.Model):
 
 # connection class Model
 class Connection (models.Model):
-    pass
+    user = models.ForeignKey(User, related_name = 'user', on_delete = models.CASCADE)
+    follow_user = models.ForeignKey(User, related_name='follow_user', on_delete=models.CASCADE)
+    

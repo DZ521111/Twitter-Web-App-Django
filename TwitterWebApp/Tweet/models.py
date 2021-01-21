7af2d884acde9934e15fc2b3c47250222c6e7c05
DZ521111 <dhruvkakadiya3@gmail.com>
2021-01-21 17:45:26 +0530

    Tweet/models.py updated!

--- TwitterWebApp/Tweet/models.py
+++ TwitterWebApp/Tweet/models.py
@@ -8,3 +8,14 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
+# tweets models
+
+class Tweets (models.Model):
+    content = models.TextField(max_length = 5000)
+    date_posted = models.DateTimeField(default = timezone.now)
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    likes= models.IntegerField(default = 0)
+    dislikes= models.IntegerField(default = 0)
+
+    def __str__(self):
+        return self.content[ : 10]

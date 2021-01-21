f78ccc1af16c7460acd701886021a4e99b34ee74
DZ521111 <dhruvkakadiya3@gmail.com>
2021-01-21 17:45:39 +0530

    Tweet/models.py updated!

--- TwitterWebApp/Tweet/models.py
+++ TwitterWebApp/Tweet/models.py
@@ -14,8 +14,8 @@ class Tweets (models.Model):
     content = models.TextField(max_length = 5000)
     date_posted = models.DateTimeField(default = timezone.now)
     author = models.ForeignKey(User, on_delete = models.CASCADE)
-    likes= models.IntegerField(default = 0)
-    dislikes= models.IntegerField(default = 0)
+    likes = models.IntegerField(default = 0)
+    dislikes = models.IntegerField(default = 0)
 
     def __str__(self):
         return self.content[ : 10]

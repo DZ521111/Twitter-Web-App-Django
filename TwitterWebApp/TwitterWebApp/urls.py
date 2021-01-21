156174c591b2f2365631c60e987817a4a6db9acf
DZ521111 <dhruvkakadiya3@gmail.com>
2021-01-21 13:33:35 +0530

    settings.py updated!

--- TwitterWebApp/TwitterWebApp/urls.py
+++ TwitterWebApp/TwitterWebApp/urls.py
@@ -28,5 +28,5 @@ urlpatterns = [
 ]
 
 # for media URLs
-#if (settings.DEBUG):
-#   urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if (settings.DEBUG):
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

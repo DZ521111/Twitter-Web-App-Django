28345ec2294e38ce8633d8d52b8acbb4f8e9b6e5
DZ521111 <dhruvkakadiya3@gmail.com>
2021-01-22 10:38:21 +0530

    Tweet/views.py file updated!

--- TwitterWebApp/Tweet/views.py
+++ TwitterWebApp/Tweet/views.py
@@ -60,3 +60,45 @@ class AllTweetView(LoginRequiredMixin, ListView):
 # about us page function
 def aboutus(request):
     return (render(request, 'aboutus.html'))
+
+class UserTweetView(LoginRequiredMixin, ListView):
+    model = Tweets
+    template_name = 'user_posts.html'
+    context_object_name = 'posts'
+    paginate_by = page_count
+
+    def visible_user(self):
+        return (get_object_or_error(User, username = self.kwargs.get('username')))
+
+    def get_context_data(self, **kwargs):
+        visible_user = self.visible_user()
+        logged_user = self.request.user
+        print(logged_user.username == '', file = sys.stderr)
+
+        if ((logged_user.username == '') or (logged_user is None)):
+            can_follow = False
+        else:
+            can_follow = (Connection.objects.filter(user = logged_user, follow_user = visible_user).count() == 0)
+
+        data = super().get_context_data(**kwargs)
+        data['user_profile'] = visible_user
+        data['can_follow'] = can_follow
+        return (data)
+
+    def get_queryset(self):
+        user = self.visible_user()
+        return Tweets.objects.filter(author = user).order_by('-date_posted')
+
+    def post(self, request, *args, **kwargs):
+        if request.user.id is not None:
+            follows_between = Connection.objects.filter(user = request.user, follow_user = self.visible_user())
+
+            if ('follow' in request.POST):
+                    new_relation = Connection(user = request.user, follow_user = self.visible_user())
+                    if (follows_between.count() == 0):
+                        new_relation.save()
+            elif ('unfollow' in request.POST):
+                    if (follows_between.count() > 0):
+                        follows_between.delete()
+
+        return (self.get(self, request, *args, **kwargs))
